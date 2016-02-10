eeb347fb82d6b06c26e96c4a945bbd57613e7ad3
DavidThcf <thcf963@hotmail.com>
2016-02-10 13:27:02 -0500

    registre

--- Project.Management/MProjectWPF/Windows/Registro/RegisterWindow.xaml.cs
+++ Project.Management/MProjectWPF/Windows/Registro/RegisterWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MProjectWPF.UserControl.Inicio;
+using MProjectWPF.Model;
 
 namespace MProjectWPF.UserControl.Registro
 {
@@ -35,7 +36,24 @@ namespace MProjectWPF.UserControl.Registro
 
         private void btn_reg_Click(object sender, RoutedEventArgs e)
         {
+            MProjectDeskSQLITEEntities dbMP = new MProjectDeskSQLITEEntities();
+           
+            usuario usu = new usuario();
+            usu.id_usuario = Convert.ToInt32(txt_ide.Text);
+            usu.e_mail = txt_email.Text;
+            usu.nombre = txt_name.Text;
+            usu.apellido = txt_lastName.Text;
+            usu.pass = txt_Password.Text;
+            try
+            {
+                dbMP.usuarios.Add(usu);
+                dbMP.SaveChanges();
+            }
+            catch(Exception err) {
+                MessageBox.Show(err.InnerException.ToString());
+            }
 
+            
         }
     }
 }
