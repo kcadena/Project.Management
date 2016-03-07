b9f80718f3339ae3408eca1f533e4cd4fd1f172c
DavidThcf <thcf963@hotmail.com>
2016-03-07 04:48:16 -0500

    Interfaz

---
+++ Project.Management/MProjectWPF/Controller/Documents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Xps.Packaging;
+using System.IO;
+using System.Windows.Controls;
+
+namespace MProjectWPF.Controller
+{
+    class Documents
+    {
+        public Documents()
+        {
+            XpsDocument doc = new XpsDocument("C:/Users/David E/Desktop/ANTEPROYECTO.xps", FileAccess.ReadWrite);
+            DocumentViewer dc = new DocumentViewer();
+            dc.Document = doc.GetFixedDocumentSequence();
+                                    
+        }
+    }
+}
--- Project.Management/MProjectWPF/MainWindow.xaml.cs
+++ Project.Management/MProjectWPF/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System.Windows;
 using MProjectWPF.Windows.Inicio;
-
-
 using MProjectWPF.UsersControlls;
 using System.Windows.Media.Animation;
 
@@ -11,20 +9,18 @@ namespace MProjectWPF
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {        
+    {
         public MainWindow()
         {
             InitializeComponent();
-            //this.Visibility = Visibility.Hidden;
+            /*this.Visibility = Visibility.Hidden;
 
-            /*LoginWindow log = new LoginWindow();            
-            log.Visibility = Visibility.Visible;*/
+            LoginWindow log = new LoginWindow();            
+            log.Visibility = Visibility.Visible; */
 
-            
-
-            viewPro.Children.Add(new ListProject(this,"LISTA PROYECTOS"));
-                      
-            
+            lal.Children.Add(new ListProject(this, "LISTA PROYECTOS"));
         }
+            
+            
     }
 }
