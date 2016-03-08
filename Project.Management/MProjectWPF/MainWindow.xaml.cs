447f42a497fecb2789a2ce733bef07d1d7ee7dfa
DavidThcf <thcf963@hotmail.com>
2016-03-08 14:02:37 -0500

    List Prject Desing

--- Project.Management/MProjectWPF/MainWindow.xaml.cs
+++ Project.Management/MProjectWPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MProjectWPF.Windows.Inicio;
 using MProjectWPF.UsersControlls;
 using System.Windows.Media.Animation;
+using System.Windows.Media;
 
 namespace MProjectWPF
 {
@@ -18,9 +19,17 @@ namespace MProjectWPF
             LoginWindow log = new LoginWindow();            
             log.Visibility = Visibility.Visible; */
 
+            lstmen.Items.Add(new LabelProject("Nuevo Proyecto", this, true));
+            lstmen.Items.Add(new LabelProject("Abrir Proyecto", this, true));
+            lstmen.Items.Add(new LabelProject("Importar Proyecto", this, true));
+
+            lstrec.Items.Add(new LabelProject("Proyectos de Ingenieria", this, true));
+            lstrec.Items.Add(new LabelProject("Desarrollo Investic ", this, true));
+            lstrec.Items.Add(new LabelProject("Acreditacion Sistemas", this, true));
+            lstrec.Items.Add(new LabelProject("Desarrollo MProject", this, true));
+            lstrec.Items.Add(new LabelProject("Aplicacion Inventario", this, true));
+
             lal.Children.Add(new ListProject(this, "LISTA PROYECTOS"));
-        }
-            
-            
+        }        
     }
 }
