ebda15392c7c51926ed1ac5cec87de8a7321ae91
DavidThcf <thcf963@hotmail.com>
2016-03-09 03:53:13 -0500

    toolbar

--- Project.Management/MProjectWPF/MainWindow.xaml.cs
+++ Project.Management/MProjectWPF/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using MProjectWPF.Windows.Inicio;
 using MProjectWPF.UsersControlls;
 using System.Windows.Media.Animation;
 using System.Windows.Media;
+using System;
+using System.Windows.Interop;
 
 namespace MProjectWPF
 {
@@ -11,17 +13,25 @@ namespace MProjectWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool admin = false;
         public MainWindow()
         {
             InitializeComponent();
             /*this.Visibility = Visibility.Hidden;
 
             LoginWindow log = new LoginWindow();            
-            log.Visibility = Visibility.Visible; */
+            log.Visibility = Visibility.Visible;*/
+            WindowInteropHelper interopHelper = new WindowInteropHelper(this);
+            IntPtr manejadorVentana = interopHelper.Handle;
 
             lstmen.Items.Add(new LabelProject("Nuevo Proyecto", this, true));
             lstmen.Items.Add(new LabelProject("Abrir Proyecto", this, true));
             lstmen.Items.Add(new LabelProject("Importar Proyecto", this, true));
+            if (admin)
+            {
+                lstmen.Items.Add(new LabelProject("Nueva Plantilla", this, true));
+            }
+            
 
             lstrec.Items.Add(new LabelProject("Proyectos de Ingenieria", this, true));
             lstrec.Items.Add(new LabelProject("Desarrollo Investic ", this, true));
@@ -29,7 +39,7 @@ namespace MProjectWPF
             lstrec.Items.Add(new LabelProject("Desarrollo MProject", this, true));
             lstrec.Items.Add(new LabelProject("Aplicacion Inventario", this, true));
 
-            lal.Children.Add(new ListProject(this, "LISTA PROYECTOS"));
+            lal.Children.Add(new ListProject(this, "LISTA PROYECTOS"));                        
         }        
     }
 }
