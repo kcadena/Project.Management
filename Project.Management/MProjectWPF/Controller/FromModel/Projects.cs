f0d94b462a9bc600768317c5bb1a587ad686720a
DavidThcf <thcf963@hotmail.com>
2016-05-10 08:41:12 -0500

    cambio

---
+++ Project.Management/MProjectWPF/Controller/FromModel/Projects.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MProjectWPF.Model;
+
+namespace MProjectWPF.Controller.FromModel
+{
+    class Projects
+    {
+        private MProjectDeskSQLITEEntities MPdb = new MProjectDeskSQLITEEntities();
+
+        public bool createProject()
+        {
+            proyecto prj = new proyecto();
+
+            return false;
+        }
+
+    }
+}
