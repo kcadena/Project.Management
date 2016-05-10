e62395c604e9d8855ab1672de450be96e90bac87
DavidThcf <thcf963@hotmail.com>
2016-05-10 08:43:51 -0500

    main

--- Project.Management/MProjectWPF/MainWindow.xaml.cs
+++ Project.Management/MProjectWPF/MainWindow.xaml.cs
@@ -46,14 +46,7 @@ namespace MProjectWPF
             lstUser.Items.Add(new LabelUser());
             lstUser.Items.Add(new LabelUser());
 
-            //LoginWindow log = new LoginWindow();            
-            //log.Visibility = Visibility.Visible;
-            Dictionary<string, long> dat= new Dictionary<string, long>();
-            dat["id"] = 1;
-            dat["car"] = 11;
-            ExplorerProject exPro = new ExplorerProject(grid_main_window,dat);
-            this.grid_main_window.Children.Add(exPro);
-            ////hokodakdiasjoidas
+       
             
         }
     }
